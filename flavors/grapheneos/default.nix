205a127e6a2625570efcea2bf66fa7b86c04a13c
Ryan Trinkle <ryan.trinkle@gmail.com>
2023-01-04 16:16:28 -0500

    Change default buildID

--- flavors/grapheneos/default.nix
+++ flavors/grapheneos/default.nix
@@ -33,7 +33,7 @@ in mkIf (config.flavor == "grapheneos") (mkMerge [
   apv.buildID = mkDefault (
     if (elem config.device [ "coral" "flame" ]) then "TP1A.221005.002"
     else if (elem config.device [ "panther" "cheetah" ]) then "TD1A.221105.001"
-    else "TQ1A.221205.011"
+    else "TQ1A.230105.002"
   );
 
   # Not strictly necessary for me to set these, since I override the source.dirs above
